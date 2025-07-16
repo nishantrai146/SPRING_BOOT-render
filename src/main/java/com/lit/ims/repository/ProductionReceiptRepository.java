a1227bf0dd7674cbe0c468223b5ea90d27ff266d
nishantrai146 <nishantgauravrvs@gmail.com>
2025-07-16 11:26:08 +0530

    endpoint to get the data in data of tabel in production floor receipt

--- src/main/java/com/lit/ims/repository/ProductionReceiptRepository.java
+++ src/main/java/com/lit/ims/repository/ProductionReceiptRepository.java
@@ -4,6 +4,12 @@ import com.lit.ims.entity.ProductionReceipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductionReceiptRepository extends JpaRepository<ProductionReceipt,Long> {
+    /* ProductionReceiptRepository.java */
+    List<ProductionReceipt> findByCompanyIdAndBranchIdOrderByReceiptDateDesc(
+            Long companyId, Long branchId);
+
 }
