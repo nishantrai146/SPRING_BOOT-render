20ba42824690fb5a5ce821a7629e775102f64168
nishantrai146 <nishantgauravrvs@gmail.com>
2025-07-28 15:27:45 +0530

    Added receipt number in wip return

--- src/main/java/com/lit/ims/dto/WipReturnDTO.java
+++ src/main/java/com/lit/ims/dto/WipReturnDTO.java
@@ -10,6 +10,7 @@ public class WipReturnDTO {
     private String transactionNumber;
     private String returnType;
     private LocalDate returnDate;
+    private String receiptNumber;
     private Long workOrderId;
     private Long warehouseId;
     private List<WipReturnItemDTO> returnItems;
